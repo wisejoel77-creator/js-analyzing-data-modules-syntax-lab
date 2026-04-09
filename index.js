b13a8792849ebfd23222de1becfebab5ef3044e2
Joel Wise <joelwise.ndungu@student.moringaschool.com>
2026-04-09 11:55:04 +0300

    clean commit

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
+require ('datejs')
+
+function combineUsers( ...args) {
+  const combinedObjects = {
+  users : []
+}
+
+args.array.forEach(element => {
+  combinedObjects.users.push (...arr)
+});
+
+combinedObject.merge_date = new Date().toString('M/d/yyyy');
+  return combinedObject;
+}
+
+const result = combineUsers(
+  ['alice', 'bob'],
+  ['charlie'],
+  ['dave', 'eve']
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
